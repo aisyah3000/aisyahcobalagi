f3e004fe536d6e2ade52ee873c1247d4d64b6e96
Ina Asiah <aisyahbgr03@gmail.com>
2025-05-20 12:56:49 +0700

    implementasi

---
+++ siswa.java
@@ -0,0 +1,16 @@
+public class siswa {
+    String nama;
+    siswa berikutnya;
+
+    // buat constructor sendiri
+    // default constructor tidak dibuat JVM
+    /*
+    public siswa(){
+
+    }
+    */
+
+    public siswa(String x){
+    nama = x;
+    }    
+}
