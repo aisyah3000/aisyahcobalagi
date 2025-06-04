c6f7555a3f9e6854061c93f49e49af9d02cbd600
aisyah3000 <aisyahbgr03@gmail.com>
2025-06-04 07:54:34 +0700

    implement mengisi stack dan menampilkan hasilnya

---
+++ stack/Tumpukan.java
@@ -0,0 +1,36 @@
+public class Tumpukan {
+
+    Barang palingAtas;
+
+    public void tumpuk(Barang b){
+        palingAtas = b;
+
+    }
+
+    public void lihatIsiTumpukan(){
+        if(palingAtas == null){
+            System.out.println("Tumpukan kosong");
+            return;
+        }
+
+        Barang sekarang = palingAtas;
+        System.out.println("Barang : "+sekarang.nama);
+
+        while(sekarang.dibawahnya != null) {
+            sekarang = sekarang.dibawahnya;
+            System.out.println("Barang : "+sekarang.nama);
+        }
+    }
+
+    public Barang ambil(){
+        return null;
+    }
+
+    public Barang lihatPalingAtas(){
+        return null;
+    }
+
+    public Boolean kosongkah(){
+        return null;
+    }
+}
